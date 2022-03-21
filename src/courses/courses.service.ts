e8bfd201803d268ea55602208c2e7b8a5e1848b4
Adroaldo <hygino@msn.com>
2022-03-21 14:40:23 -0300

    Conhecendo os métodos para tratamento de erros

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Course } from './entities/course.entity';
 
 @Injectable()
@@ -23,7 +23,14 @@ export class CoursesService {
   }
 
   findOne(id: string) {
-    return this.courses.find((course) => course.id === Number(id));
+    const course = this.courses.find((course) => course.id === Number(id));
+    if (!course) {
+      throw new HttpException(
+        `Course ID ${id} not found`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    return course;
   }
 
   create(createCourseDto: any) {
