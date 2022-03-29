a728b78d2932f1b727e4e345c3d8d5e08c54ccfa
Adroaldo Ferreira <hygino@msn.com>
2022-03-29 08:42:04 -0300

    Adicionando useState a um componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import Header from './components/Header';
 import Menu from './components/Menu';
 
 function App() {
+
+  const [counter, setCounter] = useState(1);
+
+
+
+  function handleButtonClick() {
+    setCounter(counter + 1);
+    console.log(counter);
+  }
+
   return (
     <>
-      <Header title='Página inicial'/>
-      <Header title='Página do desenvolvedor'/>
+      <Header title='Página inicial' />
+      <Header title='Página do desenvolvedor' />
       <h1>Olá Dev!</h1>
 
+      <p>{counter}</p>
+      <button onClick={ handleButtonClick }>Incrementar</button>
       <Menu>
         <ul>
           <li>Home</li>
