b4cc7aad5157d56775b2104d49c59e863c52fc8b
Adroaldo <hygino@msn.com>
2024-02-15 21:02:47 -0300

    Request params express way

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
-import { Request, Response } from 'express';
 import { CreateUserDTO } from 'src/users/dtos/CreateUser.dto';
+import { Request, Response } from 'express';
 
 @Controller('users')
 export class UsersController {
@@ -44,5 +44,11 @@ export class UsersController {
   insertUser(@Body() userData: CreateUserDTO) {
     console.log(userData);
     return {};
+  }
+
+  @Get(':id')
+  getUserById(@Req() request: Request, @Res() response: Response) {
+    console.log(request.params);
+    response.send('');
   }
 }
